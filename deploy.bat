e7e815780a16e8079734451efa536bc5d2fe6d28
Marcos Pires <mpires@Groundswellgroup.ca>
2020-01-29 10:09:55 -0700

    jenkinsfile test

--- deploy.bat
+++
@@ -1,7 +0,0 @@
-REM Build the artefacts
-call mvn clean install -q
-echo on
-REM Deploy them into servers
-echo "Starting to deploy into Prod"
-cd ./HelloWorldCompositeApplication
-call mvn clean deploy -Dmaven.deploy.skip=true -Dmaven.car.deploy.skip=false
